d2ecdb5775842e3b9e3cca12028e88751cc15506
Pedroh1510 <pedroh1510@hotmail.com>
2023-08-28 20:57:41 -0300

    add style in xlsx rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,10 @@
-use simple_xlsx_writer::WorkBook;
+use simple_xlsx_writer::{Row, WorkBook};
 use std::fs::File;
 use std::io::Write;
 mod row_xlsx;
 
 const PAGE_SIZE: i32 = 50;
-const TOTAL_RESULTS: i32 = 3000;
+const TOTAL_RESULTS: i32 = 30;
 
 fn main() {
     println!("start");
@@ -18,15 +18,20 @@ fn main() {
     let mut counter = 0;
     let url = mysql::Opts::from_url("mysql://root:mauFJcuf5dhRMQrjj@localhost:3306/base").unwrap();
     let pool = mysql::Pool::new(url).unwrap();
+    let style = workbook.create_cell_style((255, 255, 255), (125, 125, 125));
     workbook
         .get_new_sheet()
         .write_sheet(|sheet_writer| {
             loop {
                 let conn = pool.get_conn().unwrap();
-                let query_result = row_xlsx::get_row(conn, offset, PAGE_SIZE);
-                println!("1 {}, {}", offset, query_result.len());
+                let rows = row_xlsx::get_rows(conn, offset, PAGE_SIZE);
+                println!("1 {}, {}", offset, rows.len());
 
-                for row_formatted in query_result {
+                for row in rows {
+                    let mut row_formatted = Row::new();
+                    row.iter().for_each(|value| {
+                        row_formatted.add_cell((value.to_string(), &style).into())
+                    });
                     sheet_writer.write_row(row_formatted).unwrap();
                     counter += 1;
                 }
